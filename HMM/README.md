b8e16876b7a04fbfdb437cb8d67dc96094293423
KaranAgarwalla <43519370+KaranAgarwalla@users.noreply.github.com>
2020-09-19 20:08:40 +0530

    Final commit

--- HMM/README.md
+++ HMM/README.md
@@ -1,4 +1,19 @@
-# Tasks for HMM
-* Extract data for HMM parameters using corpus
-* Implement Viterbi decoding
-* Generate experimental stats as required
+# POS Tagging Using Hidden Markov Model
+
+We used NLTK's brown corpus to train and test our model. The tagset used was Universal Tagset consisting of 12 different tags. 
+
+### To Run
+
+python3 hmm.py
+
+### Pre-Processing
+
+* Add sentence beginner to annotated data
+* Replaced consecutive sequence of numbers by 0, for e.g, 123AB12 -> 0AB0
+
+> For unseen words, emission probability is taken as 1 for all tags
+
+### Results
+
+* The accuracy obtained on test data is close to 95.2%
+* We evaluated per-POS accuracy and found that words with tags having very occurrences(e.g. X) or unseen words were predictably incorrectly more often as compared to others
--- HMM/hmm.py
+++ HMM/hmm.py
@@ -1,54 +1,225 @@
-import nltk
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[35]:
+
+
+import re
 import numpy as np
+import sklearn
+from nltk.corpus import brown
 
-# Load tagged corpora
-def load_data():
-	tagged_sentences = nltk.corpus.brown.tagged_sents(tagset='universal')
-	tagged_data = []
-	for i in range(len(tagged_sentences)):
-		tagged_data.append(("^", "^"))
-		for w in tagged_sentences[i]:
-			tagged_data.append(w)
-	return tagged_data
-# Find all unique tags and count for each tag
-def extract_prob(tagged_data):
-	tags_all = []
-	for (word,tag) in tagged_data:
-		tags_all.append(tag)
+def mylog(x):
+    if x==0:
+        return -1e5
+    else:
+        return np.log10(x)
 
-	(tagset, count) = np.unique(tags_all, return_counts=True)
-	print(tagset)
-	tags_dict = {}
-	for i in range(len(tagset)):
-		tags_dict[tagset[i]] = count[i]
+class HMM:
+    
+    
+    #Preprocess dataset: train and test
+    def __init__(self, train_data, test_data):
+        
+        '''
+            train_corpus: processed train_data
+            test_corpus : processed test_data
+        '''
+        
+        self.train_corpus = []
+        for i in range(len(train_data)):
+            sent = []
+            sent.append(("^", "^"))
+            sent = sent + [(re.sub('\d{1,}', '0', w), t) for (w, t) in train_data[i]]
+            self.train_corpus.append(sent)
+        
+        self.test_corpus = []
+        for i in range(len(test_data)):
+            sent = []
+            sent.append(("^", "^"))
+            sent = sent + test_data[i]
+            self.test_corpus.append(sent)
+        
+    #Train HMM
+    def train(self):
+        '''
+            all_tags: contains all tags in the corpus
+            tags_freq: frequency of individual tags
+            tags_to_num: tags to index
+            num_to_tags: index to tags
+            all_words: all words in the corpus
+            transition: transition probabilities
+            emission: emission probabilities
+        '''
+        
+        #All tags in the train corpus
+        self.all_tags = []
+        for sent in self.train_corpus:
+            for (w, t) in sent:
+                if t not in self.all_tags:
+                    self.all_tags.append(t)
+        
+        self.all_tags.sort()
+        #Count frequency for each tag
+        self.tags_freq = {}
+        for t in self.all_tags:
+            self.tags_freq[t] = 0
+        for sent in self.train_corpus:
+            for (w, t) in sent:
+                self.tags_freq[t] += 1
+        
+        #length of corpus
+        self.total_tag = 0
+        for t in self.all_tags:
+            self.total_tag += self.tags_freq[t]
 
-	# Find all unique words
-	words_all = []
-	for (w,t) in tagged_data:
-		words_all.append(w)
-	words = np.unique(words_all)
+        #tag to number and vice-versa
+        self.tags_to_num = {}
+        self.num_to_tags = {}
+        for i in range(len(self.all_tags)):
+            self.tags_to_num[self.all_tags[i]] = i
+            self.num_to_tags[i] = self.all_tags[i]
 
-	# Find count of all (word, tag pairs)
-	word_tag_freq = {}
-	for w in words:
-		for t in tagset:
-			word_tag_freq[(w,t)] = 0
-	for (w,t) in tagged_data:
-		word_tag_freq[(w,t)] += 1
+        #All possible words in the set
+        self.all_words = set()
+        for sent in self.train_corpus:
+            for (w, t) in sent:
+                self.all_words.add(w)
 
-	# Find count of one tag following another
-	# t1 is the first tag, t2 is the following tag
-	transitions = {}
-	for t1 in tagset:
-		for t2 in tagset:
-			transitions[(t1,t2)] = 0
-	for i in range(len(tagged_data)-1):
-		transitions[(tagged_data[i][1], tagged_data[i+1][1])] += 1
+        #Count frequency for each (word, tag)
+        word_tag_freq = {}
+        for w in self.all_words:
+            for t in self.all_tags:
+                word_tag_freq[(w, t)] = 0
+        for sent in self.train_corpus:
+            for (w, t) in sent:
+                word_tag_freq[(w, t)] += 1
 
-	# Obtain the emission and transition probabilities
-	emission = word_tag_freq
-	for (w,t) in emission.keys():
-		emission[(w,t)] = emission[(w,t)]/tags_dict[t]
-	for (t1,t2) in transitions.keys():
-		transitions[(t1,t2)] = transitions[(t1,t2)]/tags_dict[t1]
-	return transitions, emission
+        #evaluate transition  counts
+        self.transition = {}
+        for t1 in self.all_tags:
+            for t2 in self.all_tags:
+                self.transition[(t1, t2)] = 0
+        for sent in self.train_corpus:
+            for i in range(len(sent)-1):
+                self.transition[(sent[i][1], sent[i+1][1])] += 1
+
+        #evaluate transition and emission probabilities
+        self.emission = {}
+        for (t1, t2) in self.transition.keys():
+            self.transition[(t1, t2)] /= self.tags_freq[t1]
+        for (w, t) in word_tag_freq.keys():
+            self.emission[(w, t)] = word_tag_freq[(w, t)]/self.tags_freq[t]
+    
+    
+    #Implements Viterbi Algorithm
+    def viterbi(self, sent):
+        '''
+            sent: input sentence
+            returns predicted tags
+        '''
+        # Replace all numbers by 0
+        sentence = [re.sub('\d{1,}', '0', w) for w in sent]
+            
+        len_sent = len(sentence)
+        len_tagset = len(self.all_tags)
+
+        #SEQSCORE and BACKPTR arrays
+        SEQSCORE = [[mylog(0) for i in range(len_sent)] for j in range(len_tagset)]
+        BACKPTR =  [[0 for i in range(len_sent)] for j in range(len_tagset)]
+
+        null_tag = self.tags_to_num["^"]
+        #initialise the null tag
+        SEQSCORE[null_tag][0] = 0
+
+        for i in range(1, len_sent):#Corresponds to a given word sentence[i]
+            for cidx, ctag in enumerate(self.all_tags):#Ending at current tag
+
+                optimal_prob = -1e9 #Includes transitional probabilites
+                optimal_tag = 0
+
+                for pidx, ptag in enumerate(self.all_tags):#Previous tag
+                    prob_k_j_i = SEQSCORE[pidx][i-1] + mylog(self.transition[(ptag, ctag)])
+                    if prob_k_j_i > optimal_prob:
+                        optimal_prob = prob_k_j_i
+                        optimal_tag = pidx
+
+                if sentence[i] in self.all_words:
+                    SEQSCORE[cidx][i] = optimal_prob + mylog(self.emission[(sentence[i], ctag)])
+                else:
+                    SEQSCORE[cidx][i] = optimal_prob
+                BACKPTR[cidx][i] = optimal_tag
+
+        #Sequence identification step
+        CT = 0
+        optimal_prob = -1e9
+        for i in range(len_tagset):
+            if SEQSCORE[i][len_sent-1]>optimal_prob:
+                optimal_prob = SEQSCORE[i][len_sent-1]
+                CT = i
+        
+        pred_tags = [CT for i in range(len_sent)]
+        for i in reversed(range(len_sent-1)):
+            pred_tags[i] = BACKPTR[pred_tags[i+1]][i+1]
+
+        pred_tags = [self.num_to_tags[idx] for idx in pred_tags]
+        return pred_tags
+    
+    def evaluation(self):
+        '''
+            Returns evaluation metrics on the train_corpus
+        '''
+        len_tagset = len(self.all_tags)
+            
+        confusion_matrix = np.zeros((len_tagset, len_tagset), dtype = np.int32)
+        
+        for sent in self.test_corpus:
+            #Untagged Sentence
+            untagged_sent = []
+            for (w, t) in sent:
+                untagged_sent.append(w)
+    
+            predicted_tags = self.viterbi(untagged_sent)
+            
+            for i in range(len(sent)):
+                confusion_matrix[self.tags_to_num[predicted_tags[i]]][self.tags_to_num[sent[i][1]]] += 1
+        
+        accuracy = np.trace(confusion_matrix)/np.sum(confusion_matrix)
+        return accuracy, confusion_matrix
+
+    
+corpora = brown.tagged_sents(tagset='universal')
+num_folds = 5
+num_tags = 13
+all_tags = {}
+subset_size = int(len(corpora)/num_folds)
+sum_confusion_matrix = np.zeros((num_tags, num_tags), dtype = np.int32)
+
+for i in range(5):
+    
+    test_data  = corpora[i*subset_size:][:subset_size]
+    train_data = corpora[:i*subset_size] + corpora[(i+1)*subset_size:]
+    hmm = HMM(train_data, test_data)
+    hmm.train()
+    accuracy, confusion_matrix = hmm.evaluation()
+    
+    sum_confusion_matrix += confusion_matrix
+    print("\nEvaluating Fold:{}".format(i+1))
+    print("Accuracy:{}".format(accuracy))
+    if i == 2:
+        all_tags = hmm.all_tags
+        print("\nConfusion Matrix for Fold:3")
+        print(confusion_matrix)
+
+accuracy = np.trace(sum_confusion_matrix)/np.sum(sum_confusion_matrix)
+accuracy_per_pos = {}
+recall_per_pos   = {}
+for idx, tag in enumerate(all_tags):
+    accuracy_per_pos[tag] = sum_confusion_matrix[idx][idx]/np.sum(sum_confusion_matrix[:, idx])
+    recall_per_pos[tag]   = sum_confusion_matrix[idx][idx]/np.sum(sum_confusion_matrix[idx, :])
+print("\nPer POS Accuracy")
+print(sorted(accuracy_per_pos.items()))
+print("\nPer POS Recall")
+print(sorted(recall_per_pos.items()))
+print("Average Accuracy:{}".format(accuracy))
+
